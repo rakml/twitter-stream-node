71fbdc687f39e17d10d04191705b4ef61c2b51fe
Rabah Kamal <Fran@Rabahs-MacBook-Pro.local>
2016-08-23 19:14:52 -0700

    Running http server with app.get

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+var express = require('express')
+	, app 	= express()
+	, http	= require('http').Server(app)
+
+app.get('/', function(req, res) {
+	res.send('Hello World')
+})
+
+app.set('port', process.env.PORT || 8080);
+http.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
